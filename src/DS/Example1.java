e8063a4339c1b2d6f1d3b99bd2d3de3739222360
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-06 07:29:07 +0000

    Added The Example1.java File

---
+++ src/DS/Example1.java
@@ -0,0 +1,14 @@
+package DS;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Example1 {
+    public static void main(String[] args){
+     Map <String,Integer> m_obj=new HashMap<>(); 
+     m_obj.put("RaVivar", 101);
+     m_obj.put("SOMVAR", 102);
+     m_obj.put("Mangayvar",103);
+     System.out.println(m_obj);
+    }
+}
