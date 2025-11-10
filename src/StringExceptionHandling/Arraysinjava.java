740538f7e7bd342c7f5eda29848552acbe9b3c49
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-10 17:21:44 +0000

    added the array file in the repo

---
+++ src/StringExceptionHandling/Arraysinjava.java
@@ -0,0 +1,10 @@
+package StringExceptionHandling;
+
+public class Arraysinjava {
+    public static void main(String[] args) {
+        int[] arr = {10,21,31,41,51}; //declaration of the array 
+        for(int i : arr){
+            System.out.println(i);
+        }
+    }
+}
