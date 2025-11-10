175f89c6cc7b39385f2b7c7a3658867352160156
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-10 08:45:56 +0000

    .

--- src/Multithreading/Multithreadexample.java
+++ src/Multithreading/Multithreadexample.java
@@ -1,7 +1,21 @@
 package Multithreading;
+class MyThread extends Thread{
+    public void run(){
+        for(int i =1; i<=5;i++){
+            System.out.println(Thread.currentThread().getName()+"is running");
+            try{
+                Thread.sleep(500);
+            }catch(InterruptedException e){
 
+            }
+        }
+    }
+}
 public class Multithreadexample {
     public static void main(String[] args) {
-        
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+        t1.start();
+        t2.start();
     }
 }
