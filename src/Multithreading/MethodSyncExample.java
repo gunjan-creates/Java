dd84b71d416d2d14cebddfc608c347cb413ed473
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-10 08:21:14 +0000

    .

--- src/Multithreading/MethodSyncExample.java
+++ src/Multithreading/MethodSyncExample.java
@@ -1,39 +1,51 @@
 package Multithreading;
-public class Table{
-    synchronized void printTable(int n){
-        for(int i = 1; i<=5;i++){
-            System.out.println(n*i);
+
+// Synchronized Method Example
+class Table {
+    synchronized void printTable(int n) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(n * i);
             try {
                 Thread.sleep(400);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            }
         }
     }
 }
-public class MyThread1 extends Thread{
+
+class MyThread1 extends Thread {
     Table t;
-    MyThread1(Table t){
-        this.t=t;
+
+    MyThread1(Table t) {
+        this.t = t;
     }
-    public void run(){
+
+    public void run() {
         t.printTable(5);
     }
 }
-public class MyThread2 extends Thread{
+
+class MyThread2 extends Thread {
     Table t;
-    MyThread2(Table t){
-        this.t=t;
+
+    MyThread2(Table t) {
+        this.t = t;
     }
-    public void run(){
+
+    public void run() {
         t.printTable(10);
     }
 }
 
 public class MethodSyncExample {
     public static void main(String[] args) {
+
         Table obj = new Table();
+
         MyThread1 t1 = new MyThread1(obj);
-        MyThread2 t2 = new MyThread2(t);
+        MyThread2 t2 = new MyThread2(obj);  // ✅ FIXED
+
         t1.start();
         t2.start();
     }
