790741e168c3b37c21e7d835a1b30dbe81768817
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-11 01:32:01 +0000

    .

--- src/StringExceptionHandling/StringManupulationExample.java
+++ src/StringExceptionHandling/StringManupulationExample.java
@@ -6,5 +6,9 @@ public class StringManupulationExample {
         System.out.println(str.toLowerCase());
         System.out.println(str.toUpperCase());
         System.out.println("original string : "+ str);
+        System.out.println(str.length());
+        System.out.println(str.trim());
+        System.out.println(str.substring(0, 8));
+        System.out.println(str.replace("gunjan", "Dhanshree"));
     }
 }
