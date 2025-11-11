648125827053b63764240fd200ed1092dfb1f9a0
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-11 05:56:01 +0000

    .

--- src/ExceptionHandelling/ControlFlowOnException.java
+++ src/ExceptionHandelling/ControlFlowOnException.java
@@ -4,6 +4,7 @@ public class ControlFlowOnException {
     public static void main(String[] args) {
         try{
             int a = Integer.parseInt("abc");
+            System.out.println(a);
         }catch(NumberFormatException e){
             System.out.println("Invalid Number");
         }finally{
