e659072ff6c909564622a59f82ea906245cb20c4
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-11 02:42:39 +0000

    just added the String buffer Program To understand how we can make an String Buffer

---
+++ src/StringExceptionHandling/StrinBufferExample.java
@@ -0,0 +1,9 @@
+package StringExceptionHandling;
+
+public class StrinBufferExample {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("hello");
+        sb.append("i am Gunjan");
+        sb.append("now learning the string buffer ");
+    }
+}
