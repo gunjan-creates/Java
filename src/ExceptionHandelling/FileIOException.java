54b110aa9143cffadccafeb7e362389d29a4181f
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-11 03:16:17 +0000

    added the java program on exception handeling on file i/o

---
+++ src/ExceptionHandelling/FileIOException.java
@@ -0,0 +1,7 @@
+package ExceptionHandelling;
+import java.io.FileReader;
+public class FileIOException {
+    public static void main(String[] args) {
+        FileReader fr = new FileReader("main.cpp");//file not found exception
+    }
+}
