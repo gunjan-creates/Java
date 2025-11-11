9ec6bd190083a8a20da19fe5093b9a45f300c0b0
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-11 03:06:02 +0000

    added the file foe the program based on the package manupuilation

---
+++ src/ExamplePackageForTheTesting/Hello.java
@@ -0,0 +1,7 @@
+package ExamplePackageForTheTesting;
+
+public class Hello {
+    public void show(){
+        System.out.println("Hello From The Class Hello " );
+    }
+}
---
+++ src/PackageTester/Test1.java
@@ -0,0 +1,7 @@
+package PackageTester;
+import ExamplePackageForTheTesting.Hello;
+public class Test1 {
+    public static void main(String[] args) {
+        new Hello().show();
+    }
+}
