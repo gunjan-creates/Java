2b2d95cedb3711d841c0f83a130dbb4feb01b98c
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-11 05:13:19 +0000

    added the control flow of the exception program !

--- src/ExceptionHandelling/ControlFlowOnException.java
+++ src/ExceptionHandelling/ControlFlowOnException.java
@@ -2,6 +2,12 @@ package ExceptionHandelling;
 
 public class ControlFlowOnException {
     public static void main(String[] args) {
-        
+        try{
+            int a = Integer.parseInt("abc");
+        }catch(NumberFormatException e){
+            System.out.println("Invalid Number");
+        }finally{
+            System.out.println("Finally Bock is Here !");
+        }
     }
 }
