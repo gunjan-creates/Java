3e8b60a7b5e778811608cefe9e81f3c4a2ae5253
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-11 01:23:15 +0000

    added the program on string manupulation

---
+++ src/StringExceptionHandling/StringManupulationExample.java
@@ -0,0 +1,10 @@
+package StringExceptionHandling;
+
+public class StringManupulationExample {
+    public static void main(String[] args) {
+        String str = "gunjan ghangare";
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+        System.out.println("original string : "+ str);
+    }
+}
