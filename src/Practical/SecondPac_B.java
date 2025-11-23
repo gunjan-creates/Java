a5ac19cec7794c969889f7b13539afc08474b765
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-23 19:59:29 +0530

    Changed the data type of the Roll No To String

--- src/Practical/SecondPac_B.java
+++ src/Practical/SecondPac_B.java
@@ -3,9 +3,9 @@ class B{
     static String college= "Rtmssu";
     String Name;
     int year;
-    Character Roll_No;
+    String Roll_No;
     int Sem;
-    B(int year,Character Roll_No,int Sem,String Name){
+    B(int year,String Roll_No,int Sem,String Name){
         this.Name = Name;
         this.Roll_No = Roll_No;
         this.year=year;
@@ -13,11 +13,11 @@ class B{
         college = "RTMNU";
     }
     void Display(){
-        System.out.println("NAme Of The Student : "+Name+"\n Roll No : "+Roll_No+"\n year : "+year+"\n Sem : "+Sem);
+        System.out.println("Name Of The Student : "+Name+"\n Roll No : "+Roll_No+"\n year : "+year+"\n Sem : "+Sem);
     }
 }
 public class SecondPac_B {
     public static void main(String[]args){
-        B b = new B(3, 0, 0, null)
+        B b = new B(3, "NIT04", 0, null)
     }
 }
