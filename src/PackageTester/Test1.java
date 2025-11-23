f5eb7dba545e778d275de2c3586279df34a48d09
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-23 16:03:44 +0530

    .

--- src/PackageTester/Test1.java
+++
@@ -1,7 +0,0 @@
-package PackageTester;
-import ExamplePackageForTheTesting.Hello;
-public class Test1 {
-    public static void main(String[] args) {
-        new Hello().show();
-    }
-}
---
+++ src/Practical/FirstPrac.java
@@ -0,0 +1,6 @@
+// create a java program based on the given requirements Demonstrate the has-a relationship
+package Practical;
+
+public class FirstPrac {
+    
+}
