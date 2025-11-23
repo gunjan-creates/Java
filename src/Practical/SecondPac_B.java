efd8ecc0a87f505c3010022f130ca8787952b408
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-23 19:58:00 +0530

    .

--- src/Practical/SecondPac_B.java
+++ src/Practical/SecondPac_B.java
@@ -1,7 +1,23 @@
 package Practical;
 class B{
-    
+    static String college= "Rtmssu";
+    String Name;
+    int year;
+    Character Roll_No;
+    int Sem;
+    B(int year,Character Roll_No,int Sem,String Name){
+        this.Name = Name;
+        this.Roll_No = Roll_No;
+        this.year=year;
+        this.Sem=Sem;
+        college = "RTMNU";
+    }
+    void Display(){
+        System.out.println("NAme Of The Student : "+Name+"\n Roll No : "+Roll_No+"\n year : "+year+"\n Sem : "+Sem);
+    }
 }
 public class SecondPac_B {
-
+    public static void main(String[]args){
+        B b = new B(3, 0, 0, null)
+    }
 }
