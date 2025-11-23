ec1a06913ef69fc5aed24d096e695cd5f9fd7446
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-23 20:26:41 +0530

    Added the condition in the program so that it can able to check the correct input

--- src/Practical/SecondPac_B.java
+++ src/Practical/SecondPac_B.java
@@ -5,19 +5,25 @@ class B{
     int year;
     String Roll_No;
     int Sem;
-    B(int year,String Roll_No,int Sem,String Name){
+    B(int year,String Roll_No,int Sem,String Name,String Input){
+        if(!Input.equals(college)){
+            System.out.println("You are not the student at "+college+" go to your college website");
+        }else{
+            System.out.println("Wellcome to RTMSSU Web Support !");
+        }
         this.Name = Name;
         this.Roll_No = Roll_No;
         this.year=year;
         this.Sem=Sem;
-        college = "RTMNU";
+        college = "RTMSSU";
     }
     void Display(){
-        System.out.println("Name Of The Student : "+Name+"\n Roll No : "+Roll_No+"\n year : "+year+"\n Sem : "+Sem);
+        System.out.println("Name Of The Student : "+Name+"\n Roll No : "+Roll_No+"\n year : "+year+"\n Sem : "+Sem+"\n College : "+college);
     }
 }
 public class SecondPac_B {
     public static void main(String[]args){
-        B b = new B(3, "NIT04", 0, null)
+        B b = new B(3, "NIT04", 3, "GUNJAN","Rtmssu");
+        b.Display();
     }
 }
