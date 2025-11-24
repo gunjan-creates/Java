45990fd0d267c60e7e860bf145521d1e7d97d77c
Gunjan Ghangare <gunjanghangare590@gmail.com>
2025-11-24 08:16:03 +0530

    Adding the new program based on the Practical 4

---
+++ src/Practical/Prac_4_A.java
@@ -0,0 +1,14 @@
+package Practical;
+interface InterPac4{
+    void start();
+}
+abstract class Car1 implements InterPac4{
+    public void start1(){
+        
+    }
+}
+public class Prac_4_A {
+    public static void main(String[] args) {
+        
+    }
+}
