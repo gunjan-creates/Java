a9a9e612b2b0b08279ffb5057d5d4342515e8f4d
Gunjan <gunjanghangare590@gmail.com>
2025-12-08 20:43:18 +0530

    Ddded the hybrid inheritance program in the directory

--- src/END_SEM/Unit_2/Inheritance/HibridInheritance.java
+++ src/END_SEM/Unit_2/Inheritance/HibridInheritance.java
@@ -1,9 +1,33 @@
 package END_SEM.Unit_2.Inheritance;
-class A{
-    void Show(){
-        
+interface A{
+    void DisplayA();
+}
+interface B{
+    void DisplayB();
+}
+class C{
+    void DisplayC(){
+        System.out.println("Inside class C " );
+    }
+}
+class D extends C implements A,B{
+    @Override
+    public void DisplayA(){
+        System.out.println("inside interface A");
+    }
+    public void DisplayB(){
+        System.out.println("inside the Interface  B ");
+    }
+    void DisplayD(){
+        System.out.println("inside the D class !");
     }
 }
 public class HibridInheritance {
-
+    public static void main(String[]args){
+        D obj = new D();
+        obj.DisplayA();
+        obj.DisplayB();
+        obj.DisplayC();
+        obj.DisplayD();
+    }
 }
