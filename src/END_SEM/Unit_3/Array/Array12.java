5e58b026859bd48f1a8bcb758d3e8c7dcb24cf48
Gunjan <gunjanghangare590@gmail.com>
2025-12-09 10:20:39 +0530

    Add java programs

---
+++ src/END_SEM/Unit_3/Array/Array12.java
@@ -0,0 +1,10 @@
+package END_SEM.Unit_3.Array;
+
+public class Array12 {
+    public static void main(String[]args){
+        int [] arry = {1,2,3,4};
+        for(int i : arry){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/END_SEM/Unit_4/List/List1.java
@@ -0,0 +1,12 @@
+package END_SEM.Unit_4.List;
+import java.util.ArrayList;
+import java.util.List;
+public class List1 {
+    public static void main(String[]args){
+        List<String> names = new ArrayList<>();
+        names.add("gunjan");
+        names.add("aryan");
+        names.add("dick");
+        System.out.println(names);
+   }
+}
